6f83bfed98a0c413220ae5c6830486a111c95ca8
lincongcong <lincongcong@tangbull.com>
2020-04-21 17:51:23 +0800

    test

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -4,29 +4,29 @@
 # basic map-reduce test
 #
 
-#RACE=
-#
-## uncomment this to run the tests with the Go race detector.
-##RACE=-race
-#
-## run the test in a fresh sub-directory.
-#rm -rf mr-tmp
-#mkdir mr-tmp || exit 1
-#cd mr-tmp || exit 1
-#rm -f mr-*
-#
-## make sure software is freshly built.
-#(cd ../../mrapps && go build $RACE -buildmode=plugin wc.go) || exit 1
-#(cd ../../mrapps && go build $RACE -buildmode=plugin indexer.go) || exit 1
-#(cd ../../mrapps && go build $RACE -buildmode=plugin mtiming.go) || exit 1
-#(cd ../../mrapps && go build $RACE -buildmode=plugin rtiming.go) || exit 1
-#(cd ../../mrapps && go build $RACE -buildmode=plugin crash.go) || exit 1
-#(cd ../../mrapps && go build $RACE -buildmode=plugin nocrash.go) || exit 1
-#(cd .. && go build $RACE mrmaster.go) || exit 1
-#(cd .. && go build $RACE mrworker.go) || exit 1
-#(cd .. && go build $RACE mrsequential.go) || exit 1
-#
-#failed_any=0
+RACE=
+
+# uncomment this to run the tests with the Go race detector.
+#RACE=-race
+
+# run the test in a fresh sub-directory.
+rm -rf mr-tmp
+mkdir mr-tmp || exit 1
+cd mr-tmp || exit 1
+rm -f mr-*
+
+# make sure software is freshly built.
+(cd ../../mrapps && go build $RACE -buildmode=plugin wc.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin indexer.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin mtiming.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin rtiming.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin crash.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin nocrash.go) || exit 1
+(cd .. && go build $RACE mrmaster.go) || exit 1
+(cd .. && go build $RACE mrworker.go) || exit 1
+(cd .. && go build $RACE mrsequential.go) || exit 1
+
+failed_any=0
 #
 ## first word-count
 #
