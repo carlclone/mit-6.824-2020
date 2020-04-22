73d0748e26f97e6b6e1ef2f01056c9654b0b9445
lincongcong <lincongcong@tangbull.com>
2020-04-22 12:16:05 +0800

    revert test code

--- src/mrapps/crash.go
+++ src/mrapps/crash.go
@@ -12,6 +12,7 @@ import "../mr"
 import crand "crypto/rand"
 import "math/big"
 import "strings"
+import "os"
 import "sort"
 import "strconv"
 import "time"
@@ -19,18 +20,15 @@ import "time"
 func maybeCrash() {
 	max := big.NewInt(1000)
 	rr, _ := crand.Int(crand.Reader, max)
-	//if rr.Int64() < 330 {
-	//	crash!
-	//os.Exit(1)
-	//} else if rr.Int64() < 660 {
-	// delay for a while.
-
-	if rr.Int64() < 660 {
+	if rr.Int64() < 330 {
+		// crash!
+		os.Exit(1)
+	} else if rr.Int64() < 660 {
+		// delay for a while.
 		maxms := big.NewInt(10 * 1000)
 		ms, _ := crand.Int(crand.Reader, maxms)
 		time.Sleep(time.Duration(ms.Int64()) * time.Millisecond)
 	}
-	//}
 }
 
 func Map(filename string, contents string) []mr.KeyValue {
